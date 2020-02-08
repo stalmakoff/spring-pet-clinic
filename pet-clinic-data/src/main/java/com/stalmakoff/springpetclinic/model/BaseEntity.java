5fb199d7755cef39411ddebe052fbfd0ff5fb3eb
Daniel <stalmakoff@protonmail.com>
2020-02-08 22:10:04 +0200

    adding base entity. closes #10

---
+++ pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.stalmakoff.springpetclinic.model;
+
+import java.io.Serializable;
+
+public abstract class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
