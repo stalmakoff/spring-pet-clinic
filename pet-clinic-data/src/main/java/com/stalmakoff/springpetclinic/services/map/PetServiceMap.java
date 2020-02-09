836e78fe3d14009c580454708e6ca0c5891b0901
Daniel <stalmakoff@protonmail.com>
2020-02-09 22:09:18 +0200

    Impl Map based Services, closes #6

---
+++ pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.stalmakoff.springpetclinic.services.map;
+
+import com.stalmakoff.springpetclinic.model.Pet;
+import com.stalmakoff.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
