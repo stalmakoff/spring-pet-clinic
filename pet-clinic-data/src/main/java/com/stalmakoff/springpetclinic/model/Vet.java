0f7d0c380ce019ff0c0d044f359a07f9483ed458
Daniel <stalmakoff@protonmail.com>
2020-03-20 21:12:48 +0200

    Added specialities, added to vet, closes #27

--- pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/model/Vet.java
@@ -1,7 +1,16 @@
 package com.stalmakoff.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
 
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
