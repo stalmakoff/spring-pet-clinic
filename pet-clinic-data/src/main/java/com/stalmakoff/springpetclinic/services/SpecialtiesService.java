cdb0c73b1164dc3c45df4ec3a530dfd61c29b027
Daniel <stalmakoff@protonmail.com>
2020-08-07 18:22:02 +0300

    Added Specialties Map Impl, closes #31

---
+++ pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.stalmakoff.springpetclinic.services;
+
+import com.stalmakoff.springpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
