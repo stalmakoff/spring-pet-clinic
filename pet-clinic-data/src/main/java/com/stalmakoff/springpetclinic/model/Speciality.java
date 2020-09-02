678d0af80d8e1181d7a5722751bcc8983f177bcb
Daniel <stalmakoff@protonmail.com>
2020-09-02 17:56:07 +0300

    updated vet to use JPA, closes #37

--- pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/stalmakoff/springpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.stalmakoff.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "special")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
